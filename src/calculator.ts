57691c128123f6cb46affc97fdf0752cdfc2ef74
Edison Lau <rness123@Edisons-MacBook-Air.local>
2024-09-13 14:28:47 -0400

    add subtract operation
    subtract operator now staged

--- src/calculator.ts
+++ src/calculator.ts
@@ -9,4 +9,8 @@ export class Calculator {
   public multiply(x: number, y: number) : number {
     return x * y;
   }
+  public subtract(x: number, y: number) : number {
+    return x - y;
+  }
+
 };
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ const result_add = calc.add(2, 3);
 const result_multiply = calc.multiply(2, 3);
 console.log(`result_add = ${result_add}`);
 console.log(`result_multiply = ${result_multiply}`);
+const result_subtract = calc.subtract(3, 2);
+console.log(`result_add = ${result_add}`);
+console.log(`result_subtract = ${result_subtract}`);
 
 
 const result_divide = calc.divide(2, 3);
